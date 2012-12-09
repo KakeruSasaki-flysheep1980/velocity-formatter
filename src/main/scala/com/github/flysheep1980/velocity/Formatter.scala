8656b3ccbff57d00d30ac8a8c128fc8cd1ddbcd0
flysheep1980 <flysheep1980@gmail.com>
2012-12-09 23:14:34 +0900

    コードの整理

--- src/main/scala/com/github/flysheep1980/velocity/Formatter.scala
+++ src/main/scala/com/github/flysheep1980/velocity/Formatter.scala
@@ -5,89 +5,52 @@ import io.Source
 import java.util.regex.Matcher
 import scala.Some
 
-class Formatter(val lineSeparator: String, val indentString: String) {
+class Formatter(val lineSeparator: String, val indentString: String) extends NodeSplitter with NodeBuilder {
+  def format(input: String): String = build(split(input))
+}
 
-  def execute(input: String): String = {
-    joinWithIndent(splitToNodes(input))
+object Formatter {
+
+  private val DefaultEncodeCharset = "utf-8"
+  private val DefaultLineSeparator = "\n"
+  private val DefaultIndentString = "\t"
+
+  def formatString(str: String, encodeCharset: String, lineSeparator: String, indentString: String): String = {
+    new Formatter(lineSeparator, indentString).format(str)
   }
 
-  protected def searchIndex(str: String, left: Char, right: Char): Int = {
-    val firstLeftIndex = str.indexOf(left)
-    var diff = 0
-    // TODO indexWhereを使ってできそう
-    val taken = str.drop(firstLeftIndex).takeWhile { c =>
-      c match {
-        case _@ a if a == left => diff += 1
-        case _@ a if a == right => diff -= 1
-        case _ => // do nothing
-      }
-      diff != 0
-    }
-    firstLeftIndex + taken.length + 1
+  def formatFile(file: File, encodeCharset: String = DefaultEncodeCharset, lineSeparator: String = DefaultLineSeparator, indentString: String = DefaultIndentString) {
+    val input = Source.fromFile(file, encodeCharset).getLines.toList.mkString(lineSeparator)
+    val formatted = formatString(input, encodeCharset, lineSeparator, indentString)
+
+    val writer = new FileWriter(file)
+    writer.write(formatted)
+    writer.flush()
+    writer.close()
   }
 
-  //    @scala.annotation.tailrec
-  protected def splitToNodes(str: String): List[Node] = {
+}
 
-    def splitHtmlNode = {
-      lazy val indexOfSplit = searchIndex(str, '<', '>')
+object NodeType extends Enumeration {
+  val LeftHtmlTag, RightHtmlTag, SingleHtmlTag, DoctypeHtmlTag, VelocitySingleComment, VelocityMultiComment, IfDirective, ElseIfDirective, ElseDirective, EndDirective, SetDirective, ParseDirective, ForeachDirective, LineSeparator, Other = Value
+}
 
-      str match {
-        case _@ s if s.startsWith("<!") => (indexOfSplit, NodeType.DoctypeHtmlTag)
-        case _@ s if s.startsWith("</") => (indexOfSplit, NodeType.RightHtmlTag)
-        case _@ s if s.startsWith("<br") | s.startsWith("<hr") | s.startsWith("<meta") => {
-          (indexOfSplit, NodeType.SingleHtmlTag)
-        }
-        case _ => (indexOfSplit, NodeType.LeftHtmlTag)
-      }
-    }
+case class Node(s: String, t: NodeType.Value) {
+  lazy val trimmed = s.trim
+}
 
-    def splitVelocityNode = {
-      lazy val indexOfSplit = searchIndex(str, '(', ')')
+trait NodeBuilder {
 
-      str match {
-        case _@ s if s.startsWith("#if") => (indexOfSplit, NodeType.IfDirective)
-        case _@ s if s.startsWith("#elseif") => (indexOfSplit, NodeType.ElseIfDirective)
-        case _@ s if s.startsWith("#foreach") => (indexOfSplit, NodeType.ForeachDirective)
-        case _@ s if s.startsWith("#set") => (indexOfSplit, NodeType.SetDirective)
-        case _@ s if s.startsWith("#parse") => (indexOfSplit, NodeType.ParseDirective)
-        case _@ s if s.startsWith("#else") => ("#else".length, NodeType.ElseDirective)
-        case _@ s if s.startsWith("#end") => ("#end".length, NodeType.EndDirective)
-        case _@ s if s.startsWith("#*") => {
-          val right = "*#"
-          (str.indexOf(right) + right.length, NodeType.VelocityMultiComment)
-        }
-        case _@ s if s.startsWith("##") => (str.indexOf(lineSeparator), NodeType.VelocitySingleComment)
-      }
-    }
+  protected def indentString: String
+  protected def lineSeparator: String
 
-    // HTML関連やVelocity関連の文字列を検索
-    val regex = List("<", "#", lineSeparator).mkString("|").r
-    regex.findFirstMatchIn(str) match {
-      case Some(m) => {
-        // 分割位置 - 文字列の最初からこのindex値までを取り出す
-        // 切り出した文字列（ノード）の種別
-        val (splitIndex, nodeType) = {
-          if (m.start != 0) {
-            (m.start, NodeType.Other)
-          } else {
-            Matcher.quoteReplacement(m.group(0)) match {
-              case _@ s if s == lineSeparator => (m.end, NodeType.LineSeparator)
-              case _@ s if s == "<" => splitHtmlNode
-              case _@ s if s == "#" => splitVelocityNode
-            }
-          }
-        }
-
-        val (elem, rest) = str.splitAt(splitIndex)
-        List(Node(elem, nodeType)) ::: splitToNodes(rest)
-
-      }
-      case None => List(Node(str, NodeType.Other))
-    }
-  }
-
-  protected def joinWithIndent(nodes: List[Node]): String = {
+  /**
+   * build nodes to indented string.
+   *
+   * @param nodes list of node
+   * @return
+   */
+  def build(nodes: List[Node]): String = {
     var indentLevel = 0
     val builder = new StringBuilder
     nodes.foreach { node =>
@@ -137,29 +100,89 @@ class Formatter(val lineSeparator: String, val indentString: String) {
 
     builder.toString
   }
-
 }
 
-object Formatter {
+trait NodeSplitter {
 
-  private val DefaultEncodeCharset = "utf-8"
-  private val DefaultLineSeparator = "\n"
-  private val DefaultIndentString = "\t"
+  protected def lineSeparator: String
 
-  def format(str: String, encodeCharset: String, lineSeparator: String, indentString: String): String = {
-    new Formatter(lineSeparator, indentString).execute(str)
+  /**
+   * split string to list of node.
+   *
+   * @param target string to split
+   * @return list of node
+   */
+  def split(target: String): List[Node] = {
+    val regex = List("<", "#", lineSeparator).mkString("|").r
+    regex.findFirstMatchIn(target) match {
+      case Some(m) => {
+        // 分割位置 - 文字列の最初からこのindex値までを取り出す
+        // 切り出した文字列（ノード）の種別
+        val (splitIndex, nodeType) = {
+          if (m.start != 0) {
+            (m.start, NodeType.Other)
+          } else {
+            Matcher.quoteReplacement(m.group(0)) match {
+              case _@ s if s == lineSeparator => (m.end, NodeType.LineSeparator)
+              case _@ s if s == "<" => splitHtml(target)
+              case _@ s if s == "#" => splitVelocityDirective(target)
+            }
+          }
+        }
+
+        val (elem, rest) = target.splitAt(splitIndex)
+        List(Node(elem, nodeType)) ::: split(rest)
+
+      }
+      case None => List(Node(target, NodeType.Other))
+    }
   }
 
-  def format(file: File, encodeCharset: String = DefaultEncodeCharset, lineSeparator: String = DefaultLineSeparator, indentString: String = DefaultIndentString): String = {
-    val input = Source.fromFile(file, encodeCharset).getLines.toList.mkString(lineSeparator)
-    this.format(input, encodeCharset, lineSeparator, indentString)
+  protected def splitHtml(target: String) = {
+    lazy val indexToSplit = indexOf(target, '<', '>')
+
+    target match {
+      case _@ s if s.startsWith("<!") => (indexToSplit, NodeType.DoctypeHtmlTag)
+      case _@ s if s.startsWith("</") => (indexToSplit, NodeType.RightHtmlTag)
+      case _@ s if s.startsWith("<br") | s.startsWith("<hr") | s.startsWith("<meta") => {
+        (indexToSplit, NodeType.SingleHtmlTag)
+      }
+      case _ => (indexToSplit, NodeType.LeftHtmlTag)
+    }
   }
 
-}
+  protected def splitVelocityDirective(target: String) = {
+    lazy val indexToSplit = indexOf(target, '(', ')')
 
-object NodeType extends Enumeration {
-  val LeftHtmlTag, RightHtmlTag, SingleHtmlTag, DoctypeHtmlTag, VelocitySingleComment, VelocityMultiComment, IfDirective, ElseIfDirective, ElseDirective, EndDirective, SetDirective, ParseDirective, ForeachDirective, LineSeparator, Other = Value
-}
-case class Node(s: String, t: NodeType.Value) {
-  lazy val trimmed = s.trim
+    target match {
+      case _@ s if s.startsWith("#if") => (indexToSplit, NodeType.IfDirective)
+      case _@ s if s.startsWith("#elseif") => (indexToSplit, NodeType.ElseIfDirective)
+      case _@ s if s.startsWith("#foreach") => (indexToSplit, NodeType.ForeachDirective)
+      case _@ s if s.startsWith("#set") => (indexToSplit, NodeType.SetDirective)
+      case _@ s if s.startsWith("#parse") => (indexToSplit, NodeType.ParseDirective)
+      case _@ s if s.startsWith("#else") => ("#else".length, NodeType.ElseDirective)
+      case _@ s if s.startsWith("#end") => ("#end".length, NodeType.EndDirective)
+      case _@ s if s.startsWith("#*") => {
+        val right = "*#"
+        (s.indexOf(right) + right.length, NodeType.VelocityMultiComment)
+      }
+      case _@ s if s.startsWith("##") => (s.indexOf(lineSeparator), NodeType.VelocitySingleComment)
+    }
+  }
+
+  protected def indexOf(target: String, left: Char, right: Char): Int = {
+    val firstLeftIndex = target.indexOf(left)
+    var diff = 0
+    // TODO indexWhereを使ってできそう
+    val taken = target.drop(firstLeftIndex).takeWhile { c =>
+      c match {
+        case _@ a if a == left => diff += 1
+        case _@ a if a == right => diff -= 1
+        case _ => // do nothing
+      }
+      diff != 0
+    }
+    firstLeftIndex + taken.length + 1
+  }
+
 }
