4f229b67a08772e1db9da23b87825dfb72503ed4
sasaki <sasaki@furyu.jp>
2012-12-12 12:30:19 +0900

    format時のロギング修正

--- plugin/src/main/scala/com/github/flysheep1980/velocity/plugin/VelocityFormatterPlugin.scala
+++ plugin/src/main/scala/com/github/flysheep1980/velocity/plugin/VelocityFormatterPlugin.scala
@@ -19,7 +19,7 @@ object VelocityFormatterPlugin extends Plugin {
   def velocityFormatTask = (velocitySourceDirectory, velocitySources, streams) map {
     (dir, src, s) =>
       {
-        s.log.info("format [%d] velocity template files in [%s]".format(src.length, dir.get.map(_.getPath).mkString(",")))
+        s.log.info("format %d velocity template files in [%s]".format(src.length, dir.get.map(_.getPath).mkString(",")))
         src.get foreach { f =>
           s.log.info("Formatting [%s]".format(f.getPath))
           com.github.flysheep1980.velocity.Formatter.formatFile(f)
