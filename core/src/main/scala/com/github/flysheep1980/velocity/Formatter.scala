5f52a0c83666c21e1d7d297a938802820c9bd4fd
sasaki <sasaki@furyu.jp>
2012-12-12 18:53:43 +0900

    imgタグのparse対応

--- core/src/main/scala/com/github/flysheep1980/velocity/Formatter.scala
+++ core/src/main/scala/com/github/flysheep1980/velocity/Formatter.scala
@@ -163,7 +163,7 @@ trait NodeSplitter {
       }
       case _@ s if s.startsWith("<!") => (indexToSplit, NodeType.DoctypeHtmlTag)
       case _@ s if s.startsWith("</") => (indexToSplit, NodeType.RightHtmlTag)
-      case _@ s if s.startsWith("<br") | s.startsWith("<hr") | s.startsWith("<meta") => {
+      case _@ s if s.startsWith("<br") | s.startsWith("<hr") | s.startsWith("<meta") | s.startsWith("<img") => {
         (indexToSplit, NodeType.SingleHtmlTag)
       }
       case _ => (indexToSplit, NodeType.LeftHtmlTag)
